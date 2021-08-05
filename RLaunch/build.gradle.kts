39b51b9f5feb12127febd6abc0d1553a019b0cb6
Adam <zenithrsps345@gmail.com>
2021-08-06 00:23:51 +0100

    Project Setup

---
+++ RLaunch/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.21"
+}
+
+group = "org.launch"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
